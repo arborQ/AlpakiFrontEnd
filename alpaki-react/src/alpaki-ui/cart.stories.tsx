f770de050b5049b03c7f2252ace8c839cbbd0a82
arborQ <arbor@o2.pl>
2021-08-18 14:50:36 +0200

    work storybook

---
+++ alpaki-react/src/alpaki-ui/cart.stories.tsx
@@ -0,0 +1,17 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { Cart } from './cart';
+
+export default {
+  title: 'Example/Cart',
+  component: Cart,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Cart>;
+
+const Template: ComponentStory<typeof Cart> = (args) => <Cart {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    children: "test primary"
+};
